84d2f9b417069dbd9a80557e3ed60a2f2dac717a
abaz <no.sayers.original@gmail.com>
2023-01-15 23:18:02 +0100

    Update User.java

--- WebApplication1/src/java/Model/User.java
+++ WebApplication1/src/java/Model/User.java
@@ -15,8 +15,39 @@ public class User {
         this.password = password;
         this.admin = admin;
         this.userIsInQueue = false;
-        
     }
     
+    public String getPin(){
+        return this.pin;
+    }
+    public String getUsername(){
+        return this.username;
+    }
+    public String getPassword(){
+        return this.password;
+    }            
+    public Boolean getAdmin(){
+        return this.admin;
+    }
+    public Boolean getUserIsInQueue(){
+        return this.userIsInQueue;
+    }
+    
+    public void setPin(String pin){
+        this.pin = pin;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }   
+    public void setPassword(String password){
+        this.password = password;
+    }            
+    public void setAdmin(Boolean admin){
+        this.admin = admin;
+    }
+    public void setUserIsInQueue(Boolean userIsInQueue){
+        this.userIsInQueue = userIsInQueue;
+    }
+                
 }
 
